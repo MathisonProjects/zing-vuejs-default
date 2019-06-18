65962b66146d1c97526997f8eba615487ba81dff
Jacob Mathison <jacob@mathisonprojects.com>
2019-06-18 17:56:42 -0500

    More file planning...

---
+++ build/pages/site/FaqComponent.vue
@@ -0,0 +1,16 @@
+<template>
+	<div></div>
+</template>
+<script>
+export default {
+	name      : "",
+	components: {},
+	methods   : {},
+	computed  : {},
+	watch     : {},
+	created() {}
+};
+</script>
+<style scoped>
+
+</style>
