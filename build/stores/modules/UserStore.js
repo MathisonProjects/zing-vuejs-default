deaa0985fd974e85b4a7634426770f3e37745af8
Jacob Mathison <jacob@mathisonprojects.com>
2019-07-01 08:38:47 -0500

    Added JWT Passport package. Needs to be implemented properly. User store added that can integrate with profiles-zing.

--- build/stores/modules/UserStore.js
+++ build/stores/modules/UserStore.js
@@ -1,6 +1,102 @@
 export default {
-	state     : {},
-	getters   : {},
-	mutations : {},
-	actions   : {}
-}
+	namespaced: true,
+	state: {
+		isLoaded: false,
+		user    : '',
+		data    : {
+			stripe: null
+		},
+		token   : ''
+	},
+	mutations: {
+		SET_USER(state, payload) {
+			state.user = payload;
+		},
+		SET_TOKEN(state, payload) {
+			state.token = payload;
+		},
+		SET_LOAD(state, payload) {
+			state.isLoaded = payload;
+		},
+		SET_DATA(state, payload) {
+			state.data[payload.type] = payload.data;
+		}
+	},
+	actions: {
+		setUser({commit, dispatch}, payload) {
+			if (payload != null && payload != 'null') {
+				commit('SET_TOKEN', payload);
+				dispatch('GetUser');
+			} else {
+				commit('SET_LOAD', true);
+			}
+		},
+		GetUser({commit,dispatch,state}) {
+			axios.defaults.baseURL = 'https://profiles.zing.land';
+			config = {
+				headers: { 'Authorization' : 'Bearer '+ state.token }
+			};
+			params = { };
+
+			axios.get('/api/v1/user', config).then(response => {
+               	commit('SET_USER', response.data);
+               	dispatch('GetStripeData');
+               	commit('SET_LOAD', true);
+            });
+		},
+		LoginUser({commit,dispatch}, payload) {
+			axios.defaults.baseURL = 'https://profiles.zing.land';
+			return axios.post('/api/v1/login', payload).then(response => {
+               	if (response.data.token) {
+		    		sessionStorage['user_session'] = response.data.token;
+
+               		dispatch('setUser', response.data.token);
+	               	return 'success';
+               	} else {
+               		return 'fail';
+               	}
+            }).catch(error => {
+			    console.log(error.response)
+			});
+		},
+		RegisterUser({commit,dispatch}, payload) {
+			axios.defaults.baseURL = 'https://profiles.zing.land';
+			axios.post('/api/v1/register', payload).then(response => {
+				commit('SET_TOKEN', response.data.token);
+				dispatch('GetUser');
+			});
+		},
+		GetStripeData({commit, state}) {
+			axios.defaults.baseURL = 'https://' + window.location.hostname;
+			axios.post('/api/v1/external/getStripeUser', { email : state.user.email}).then(response => {
+				commit('SET_DATA', {
+					type: 'stripe',
+					data: response.data
+				});
+			});
+		},
+		logout({commit}) {
+			commit('SET_LOAD', false);
+		    sessionStorage['user_session'] = null;
+			commit('SET_USER', '');
+			commit('SET_DATA', { stripe : null });
+			commit('SET_TOKEN', '');
+			commit('SET_LOAD', true);
+		}
+	},
+	getters: {
+		user    : state => {
+			return state.user
+		},
+		loggedIn : state => {
+			if (state.user == '') {
+				return false;
+			} else {
+				return true;
+			}
+		},
+		isLoaded: state => {
+			return state.isLoaded
+		}
+	}
+};
