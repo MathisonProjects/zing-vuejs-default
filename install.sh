1a37d1ec95f7c57347cfa8b03790048f7c945e5d
Jacob Mathison <jacob@mathisonprojects.com>
2019-07-14 15:56:57 -0500

    Updated the install script to be easier to upgrade

--- install.sh
+++ install.sh
@@ -2,42 +2,46 @@
 
 YOURDIR="$PWD"
 
-echo "What is your environment? (DEV/PROD)"
-read environment_mix_env
+#Set Questions
+QUESTION[0]="What is your environment? (DEV/PROD)"
+QUESTION[1]="What will be your home page? (1=landing/2=sales/3=form)"
+QUESTION[2]="What is your twitter?"
+QUESTION[3]="Is this projects use other components? (t=True,f=false)"
+QUESTION[4]="What is your what is the directory name used below? (You may leave it blank)"
+QUESTION[5]="What is your database for data.zing.land?"
+QUESTION[6]="What is your application name?"
 
-echo "What will be your home page? (1=landing/2=sales/3=form)"
-read environment_mix_home
+#Set Answers
+for ((i=0; i<=6; i++)); do
+  echo "${QUESTION[${i}]}"
+  read ANSWER[${i}]
+done
 
-echo "What is your twitter?"
-read environment_mix_twitter
-
-echo "Is this projects use other components? (t=True,f=false"
-read environment_mix_custom
-
-if [ "$environment_mix_custom" == "t" ]; then
-	echo "What is your what is the directory name used below?"
-	read environment_mix_folder
-fi
+#Create .env
 
 echo > ${YOURDIR}/.env
 
-echo "MIX_ENV=${environment_mix_env}" >> ${YOURDIR}/.env
-if [ "$environment_mix_home" == 1 ]; then
+echo "MIX_ENV=${ANSWER[0]}" >> ${YOURDIR}/.env
+echo "MIX_TWITTER=${ANSWER[2]}" >> ${YOURDIR}/.env
+echo "MIX_DB=${ANSWER[5]}" >> ${YOURDIR}/.env
+echo "MIX_APP_NAME=${ANSWER[6]}" >> ${YOURDIR}/.env
+
+
+if [ "${ANSWER[1]}" == 1 ]; then
 	echo "MIX_HOME=landing" >> ${YOURDIR}/.env
 fi
-if [ "$environment_mix_home" == 2 ]; then
+if [ "${ANSWER[1]}" == 2 ]; then
 	echo "MIX_HOME=sales" >> ${YOURDIR}/.env
 fi
-if [ "$environment_mix_home" == 3 ]; then
+if [ "${ANSWER[1]}" == 3 ]; then
 	echo "MIX_HOME=form" >> ${YOURDIR}/.env
 fi
-echo "MIX_TWITTER=${environment_mix_twitter}" >> ${YOURDIR}/.env
 
-if [ "$environment_mix_custom" == "t" ]; then
+if [ "${ANSWER[3]}" == "t" ]; then
 	echo "MIX_CUSTOM=true" >> ${YOURDIR}/.env
-	echo "MIX_FOLDER=${environment_mix_folder}" >> ${YOURDIR}/.env
+	echo "MIX_FOLDER=${ANSWER[4]}" >> ${YOURDIR}/.env
 fi
-if [ "$environment_mix_custom" == "f" ]; then
+if [ "${ANSWER[3]}" == "f" ]; then
 	echo "MIX_CUSTOM=false" >> ${YOURDIR}/.env
 	echo "MIX_FOLDER=false" >> ${YOURDIR}/.env
 fi
